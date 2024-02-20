931d12004d84a892daee472f40b765118fb2f66d
Robert Nash <r.nash1@me.com>
2024-02-20 14:16:36 +0000

    Moving package.

--- webauthn-models-swift/Package.swift
+++ webauthn-models-swift/Package.swift
@@ -15,16 +15,28 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../base64-swift"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.6.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
     ],
     targets: [
         .target(
+            name: "Base64Swift",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log")
+            ]
+        ),
+        .target(
             name: "WebAuthnModels",
             dependencies: [
-                .product(name: "Base64Swift", package: "base64-swift"),
+                .target(name: "Base64Swift"),
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "_CryptoExtras", package: "swift-crypto")
+            ]
+        ),
+        .testTarget(
+            name: "Base64SwiftTests",
+            dependencies: [
+                .target(name: "Base64Swift")
             ]
         )
     ]
